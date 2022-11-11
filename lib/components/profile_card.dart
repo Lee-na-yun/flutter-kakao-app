e7925ac492ff14b3f283282084af111c656e79ec
Lee-na-yun <happyin3333@nate.com>
2022-11-11 12:43:48 +0900

    Navigator.push로 프로필화면 전환하기

--- lib/components/profile_card.dart
+++ lib/components/profile_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kakao/models/user.dart';
+import 'package:flutter_kakao/screens/profile_screen.dart';
 
 class ProfileCard extends StatelessWidget {
   const ProfileCard({required this.user, Key? key}) : super(key: key);
@@ -8,7 +9,14 @@ class ProfileCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProfileScreen(user: user),
+          ),
+        );
+      },
       child: ListTile(
         leading: CircleAvatar(
           radius: 20,
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kakao/models/user.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key, required this.user}) : super(key: key);
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("ProfileScreen"),
+      ),
+    );
+  }
+}
