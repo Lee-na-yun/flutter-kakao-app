4a9af99819dda2b07ec04c38404cccbc89dccf2b
Lee-na-yun <happyin3333@nate.com>
2022-11-11 14:29:35 +0900

    프내프로필 & 친구프로필 화면 완성

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/components/bottom_icon_button.dart';
 import 'package:flutter_kakao/models/user.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../components/round_icon_button.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key, required this.user}) : super(key: key);
@@ -7,10 +10,84 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("ProfileScreen"),
+    return Container(
+      width: 110,
+      height: 110,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(user.backgroundImage),
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: IconButton(
+            icon: Icon(
+              FontAwesomeIcons.times,
+              size: 30,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          actions: [
+            RoundIconButton(icon: FontAwesomeIcons.gift),
+            SizedBox(width: 15),
+            RoundIconButton(icon: FontAwesomeIcons.cog),
+            SizedBox(width: 20),
+          ],
+        ),
+        body: Column(
+          children: [
+            Spacer(),
+            SizedBox(height: 8),
+            Text(
+              user.name,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 5),
+            Text(
+              user.intro,
+              style: TextStyle(color: Colors.white, fontSize: 15),
+              maxLines: 1,
+            ),
+            SizedBox(height: 20),
+            Divider(color: Colors.white),
+            if (user.name == me.name) _buildMyIcons() else _buildFriendIcons(),
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget _buildMyIcons() {
+  return Padding(
+    padding: EdgeInsets.symmetric(vertical: 18),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BottomIconButton(icon: FontAwesomeIcons.comment, text: "나와의 채팅"),
+        SizedBox(width: 50),
+        BottomIconButton(icon: FontAwesomeIcons.pen, text: "프로필 편집"),
+      ],
+    ),
+  );
+}
+
+Widget _buildFriendIcons() {
+  return Padding(
+    padding: EdgeInsets.symmetric(vertical: 18),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BottomIconButton(icon: FontAwesomeIcons.comment, text: "1:1 채팅"),
+        SizedBox(width: 50),
+        BottomIconButton(icon: FontAwesomeIcons.phone, text: "통화하기"),
+      ],
+    ),
+  );
 }
