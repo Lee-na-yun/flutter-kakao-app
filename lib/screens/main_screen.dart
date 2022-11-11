12158a909266b3a678f90b7c34e53da14580996b
Lee-na-yun <happyin3333@nate.com>
2022-11-11 10:04:56 +0900

    메인 하단 아이콘 생성 및 아이콘 클릭이벤트 설정 완료

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -8,11 +9,45 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("MainScreen"),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          Center(child: Text("IndexStack 1")),
+          Center(child: Text("IndexStack 2")),
+          Center(child: Text("IndexStack 3")),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.grey[100],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.user),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.comment),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.ellipsisH),
+            label: "",
+          ),
+        ],
       ),
     );
   }
